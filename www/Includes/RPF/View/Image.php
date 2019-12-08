15b58944157bdd13a57f79d90ae9010414d0edfe
Anatoly Mikhaylichenko <greentracery@gmail.com>
2019-12-08 18:55:57 +0400

    Add cache for images

--- www/Includes/RPF/View/Image.php
+++ www/Includes/RPF/View/Image.php
@@ -21,11 +21,18 @@ class RPF_View_Image extends RPF_View
 		$out = $this->_params;
 		if(!isset($out['image_mime_type'], $out['image_size'], $out['image_data'])) exit(1);
 		
-		header("Cache-Control: no-store, no-cache, must-revalidate"); 
-		header("Expires: " .  date("r"));
+		$etag = md5($out['image_data']);
+		$filename = (isset($out['image_filename']))? 
+			$out['image_filename'] : $etag.'.'.$out['image_mime_type'];
+		
+		//header("Cache-Control: no-store, no-cache, must-revalidate"); 
+		header("Pragma: public");
+		header("Etag: ".$etag);
+		header("Expires: " .  date("r", time() + 300));
 		header("X-Server: RPF");
 		header("Content-Type: image/".$out['image_mime_type']."; ");
 		header("Content-Length: ".$out['image_size']);
+		header('Content-disposition: filename="'.$filename.'"');
 		echo $out['image_data'];
 		exit(0);
 	}
